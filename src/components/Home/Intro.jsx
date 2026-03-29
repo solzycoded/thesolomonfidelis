ea9371988bca8d329e55bddbe9f8d2ba34417b32
solzycoded <solomonfidelis012@gmail.com>
2026-03-29 21:57:01 +0100

    feat: update portfolio details and images in index.html and Intro component

--- src/components/Home/Intro.jsx
+++ src/components/Home/Intro.jsx
@@ -7,13 +7,14 @@ function Intro(){
                 <div className="row">
                     {/* sm screen */}
                     <div className="intro-sm col-12 col-sm-12">
-                        <div className="border border-2 p-2 rounded intro-sm-container">
+                    {/* border border-2  */}
+                        <div className="p-2 rounded intro-sm-container">
                             <div className="intro-sm-img-container">
-                                <img className="w-100 img-fluid rounded" src="/imgs/me.jpg" alt="solomon fidelis" />
+                                <img className="w-100 img-fluid rounded" src="/imgs/dp 1.jpeg" alt="solomon fidelis" />
                             </div>
                             <div className="mb-4">
-                                <h1>Solomon <small className="fw-lighter">"solzy"</small> Fidelis</h1>
-                                <h4 className="app-color">Software Developer & Scrum Master</h4>
+                                <h1>Solomon Fidelis</h1>
+                                <h4 className="app-color">Software Developer | Agile Practitioner</h4>
                             </div>
 
                             <div>
@@ -25,14 +26,14 @@ function Intro(){
                     <div className="col-6 col-sm-5 col-md-6 col-lg-7 position-relative intro-lg">
                         <div className="intro-message">
                             <h1>Hi, I'm Solomon Fidelis</h1>
-                            <h4>a <b className="app-color">Software Developer</b> & <b className="app-color">Scrum Master</b>.</h4>
+                            <h4>a <b className="app-color">Software Developer</b> | <b className="app-color">Agile Practitioner</b>.</h4>
                             <div className="mt-4">
                                 <a href="#about" className="btn rounded-pill read-about-me app-btn-color-outline pe-4 ps-4">Read about me</a>
                             </div>
                         </div>
                     </div>
                     <div className="col-6 col-sm-7 col-md-6 col-lg-5 intro-img-container intro-lg">
-                        <img className="w-100 img-fluid" src="/imgs/me.jpg" alt="solomon fidelis" />
+                        <img className="w-100 img-fluid" src="/imgs/dp 1.jpeg" alt="solomon fidelis" /> 
                     </div>
                 </div>
             </div>
